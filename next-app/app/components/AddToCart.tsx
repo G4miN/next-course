aa452a0a4eabff8efa379385c4f36621c3a9a162
Yeduin <el_yeduin_02@hotmail.com>
2025-06-11 19:50:53 -0400

    client and server components

---
+++ next-app/app/components/AddToCart.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <button
+      onClick={() => {
+        console.log("Button clicked!");
+      }}
+    >
+      Add to cart
+    </button>
+  );
+};
+
+export default AddToCart;
