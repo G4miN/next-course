285d9dc6400fcba69077871dd50b6e90b9115459
yeduin <el_yeduin_02@hotmail.com>
2025-06-11 16:19:57 -0400

    leftover

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <>
+      <div>UsersPage</div>
+      <Link href="/users/new">➡️ new</Link>
+    </>
+  );
 };
 
 export default UsersPage;
